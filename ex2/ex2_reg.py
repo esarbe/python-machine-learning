30c22604861f228b0231ec97d0bcd90a5a21fd9f
Raphael Bosshard <raphael.bosshard@gmail.com>
2016-01-13 15:07:10 +0100

    implement regularized gradient

--- ex2/ex2_reg.py
+++ ex2/ex2_reg.py
@@ -26,13 +26,22 @@ def map_features(X1, X2, degree=6):
 
 
 def cost_function(theta, X, y, λ):
-    [m, n] = X.shape
     theta = np.matrix(theta)
+    [m, n] = X.shape
     cost = (1 / m ) * (-y.T * np.log(sigmoid(X * theta.T)) - ( 1 - y ).T * np.log( 1 - sigmoid(X * theta.T)))
     cost = cost.A1
-    reg_cost = cost + (λ / 2 / m) * np.sum(np.power(theta[1:], 2))
+    reg_cost = cost + (λ / 2 / m) * np.sum(np.power(theta[:,1:], 2))
     return reg_cost
 
+
+def gradient_function(theta, X, y, λ):
+    theta = np.matrix(theta)
+    [m, n] = X.shape
+    gradient = ((sigmoid(X * theta.T) - y).T * X).T / m;
+    theta[0,0] = 0
+    reg = (λ / m) * theta
+    gradient_reg = gradient + reg.T
+    return gradient_reg
 
 data = np.matrix(np.loadtxt('ex2data2.txt', delimiter=','))
 
@@ -48,8 +57,12 @@ X = map_features(X[:,0], X[:,1])
 [m, n] = X.shape
 
 λ = 1
-theta = np.zeros(n)
+theta = np.zeros([1, n])
 
 cost = cost_function(theta, X, y, λ)
+grad = gradient_function(theta, X, y, λ)
+
+
+print("Cost at initial theta (zeros):", cost, ", ", grad)
 
 print(cost)
