e2df7436aa5358922ef79f57a618a3ef0bf343a5
Raphael Bosshard <raphael.bosshard@gmail.com>
2016-01-16 22:35:07 +0100

    render first image from data

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,13 +1,20 @@
 import numpy as np
 import scipy.io as sio
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+def plot_data(X):
+    padding = 1
+    plt.imshow(X[0,:].reshape([20, 20]), cmap = cm.Greys_r)
+    plt.show()
 
 input_layer_size = 400
 num_labels = 10
 
-
 # part 1: loading and visualization
 data = sio.loadmat('ex3data1.mat')
 
-m, n = data['X'].shape
+X = data['X']
+m, n = X.shape
 
-print(m)
+plot_data(X)
