6b88916120d09db291da0c6bc4a1d580f6dc1106
Raphael Bosshard <raphael.bosshard@gmail.com>
2016-01-18 19:18:43 +0100

    implement one vs. all logistic regression

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -2,7 +2,45 @@ import numpy as np
 import scipy.io as sio
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from scipy import optimize
 import math
+
+
+def logistic_regression_cost_function(theta, X, y, λ):
+    theta = np.matrix(theta)
+    [m, n] = X.shape
+    cost = (1 / m ) * (-y.T * np.log(sigmoid(X * theta.T)) - ( 1 - y ).T * np.log( 1 - sigmoid(X * theta.T)))
+    cost = cost.A1
+    reg_cost = cost + (λ / 2 / m) * np.sum(np.power(theta[:,1:], 2))
+    return reg_cost[0]
+
+
+def logistic_regression_gradient_function(theta, X, y, λ):
+    theta = np.matrix(theta)
+    [m, n] = X.shape
+    gradient = ((sigmoid(X * theta.T) - y).T * X).T / m;
+    theta[0,0] = 0
+    reg = (λ / m) * theta
+    gradient_reg = gradient + reg.T
+    return gradient_reg.A1
+
+
+def one_vs_all(X, y, num_labels, λ):
+    thetas = []
+    m, n = X.shape
+    initial_theta = np.matrix(np.zeros([n, 1]))
+    for label in range(num_labels):
+        theta = optimize.fmin_bfgs(
+            logistic_regression_cost_function,
+            initial_theta,
+            logistic_regression_gradient_function,
+            args = (X, y, λ))
+        thetas.append(theta)
+
+    return thetas
+
+def sigmoid(z):
+    return 1 / ( 1 + np.power(np.e, -z))
 
 def plot_data(X, item_width=None):
     [m, n] =  X.shape
@@ -37,12 +75,19 @@ def plot_data(X, item_width=None):
 
 input_layer_size = 400
 num_labels = 10
+λ = 0.1
 
 # part 1: loading and visualization
 data = sio.loadmat('ex3data1.mat')
 
 
 X = data['X']
+y = data['y']
 m, n = X.shape
 
-plot_data(np.random.permutation(X)[:49,:])
+plot_data(np.random.permutation(X)[:100,:])
+
+# part2:
+
+thetas = one_vs_all(X, y, num_labels, λ)
+print(np.matrix(thetas).shape)
