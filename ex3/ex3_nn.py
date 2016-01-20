9f2cda2c1d8f8b810c10a560feeb5af7a62b4bfb
Raphael Bosshard <raphael.bosshard@gmail.com>
2016-01-20 19:06:00 +0100

    implement NN prediction

---
+++ ex3/ex3_nn.py
@@ -0,0 +1,74 @@
+import scipy.io as sio
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+def plot_data(X, item_width=None):
+    [m, n] =  X.shape
+    if not item_width:
+        item_width = np.round(np.sqrt(n))
+
+    item_height = (n / item_width)
+
+    pad = 1
+
+    # grid of items to display
+    rows = math.floor(np.sqrt(m))
+    cols = math.ceil(m / rows)
+
+    display_array = - np.ones([pad + rows * (item_height + pad),
+                              pad + rows * (item_width  + pad)])
+
+    current_item = 0
+    for row in range(rows):
+        for col in range(cols):
+            offset_x = pad + row * (item_width + pad)
+            offset_y = pad + col * (item_height + pad)
+
+            data = X[current_item,:].reshape([item_height, item_width])
+            t = display_array[offset_x:offset_x + item_width, offset_y: offset_y + item_height]
+            display_array[offset_x:offset_x + item_width, offset_y: offset_y + item_height] = data
+
+            current_item = current_item + 1
+
+    plt.imshow(display_array.T, cmap = cm.Greys_r)
+    plt.show()
+
+def sigmoid(z):
+    return 1 / ( 1 + np.power(np.e, -z))
+
+def predict(Theta1, Theta2, X):
+    m, n = X.shape
+
+    # first layer
+    X0i = np.hstack([np.ones([m, 1]), X])
+    X0o = sigmoid(X0i * Theta1.T)
+
+    # second layer
+    X1i = np.hstack([np.ones([m, 1]), X0o])
+    X1o = sigmoid(X1i * Theta2.T)
+
+    # output layer
+    return np.argmax(X1o, axis=1) + 1 # account for python's 0-based index
+
+# part 1: loading and visualization
+data = sio.loadmat('ex3data1.mat')
+
+X = np.matrix(data['X'])
+y = np.matrix(data['y'])
+m, n = X.shape
+
+plot_data(np.random.permutation(X)[:100,:])
+
+# part 2: loading NN parameters and implement predict
+weights = sio.loadmat('ex3weights.mat')
+
+Theta1 = weights['Theta1']
+Theta2 = weights['Theta2']
+
+pred = predict(Theta1, Theta2, X)
+
+print("Training set accuracy", np.mean((pred == y).astype(float)) * 100)
+
+
